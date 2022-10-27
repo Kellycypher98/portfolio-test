d1153f67d0954f5f9f61e582c0b0f318349c0ff9
Kellycypher98 <kelvinboateng94@outlook.com>
2022-10-27 11:54:24 +0000

    TEST

--- form.php
+++ form.php
@@ -1,27 +1,41 @@
 <?php
-if(!empty($_POST["submit"])){
-    $firstName = $_POST["firstName"];
-    $lastName = $_POST["lastName"];
-    $emailSender = $_POST["emailSender"];
-    $telephoneSender = $_POST["telephoneSender"];
-    $service_type = $_POST["service_type"];
-    $have_web = $_POST["have_web"];
-    $nature = $_POST["nature"];
-    $budget = $_POST["budget"];
-    $toEmail= "kelvinboateng94@outlook.com";
+if(isset($_POST["firstName"]) && $_POST ["lastName"] && $_POST["emailSender"] != " "){
+    if(filter_var($_POST["emailSender"], FILTER_VALIDATE_EMAIL)){
 
-    $mailHeaders = "First Name: " . $firstName .
-    "\r\n last name: " . $lastName . 
-    "\r\n email: " . $emailSender . 
-    "\r\n telephone: " . $telephoneSender . 
-    "\r\n service type: " . $service_type . 
-    "\r\n have a website ?: " . $have_web . 
-    "\r\n nature of project: " . $nature . 
-     "\r\n budget: " . $budget .  "\r\n";
+         // submit form
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $emailSender = $_POST["emailSender"];
+        $telephoneSender = $_POST["telephoneSender"];
+        $service_type = $_POST["service_type"];
+        $have_web = $_POST["have_web"];
+        $nature = $_POST["nature"];
+        $budget = $_POST["budget"];
 
-    if(mail($toEmail,$firstName,$lastName,$mailHeaders)){
-        $message = "Your request has been sent succesfully";
+        $to = "kelvinboateng94@outlook.com";
+        
+
+        $mailHeaders = "First Name: " . $firstName .
+        "\r\n last name: " . $lastName . 
+        "\r\n email: " . $emailSender . 
+        "\r\n telephone: " . $telephoneSender . 
+        "\r\n service type: " . $service_type . 
+        "\r\n have a website ?: " . $have_web . 
+        "\r\n nature of project: " . $nature . 
+        "\r\n budget: " . $budget .  "\r\n";
+
+        mail($to,$firstName,$lastName,$mailHeaders);
+
+        
+
+
+
+
     }
+    
+   
+    
 
 }
+
 ?>
